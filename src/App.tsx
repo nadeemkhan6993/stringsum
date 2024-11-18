cedf163785032d2c169707e2b95b59285ab10e56
Nadeem Akram <nadeemkhan6993@gmail.com>
2024-11-18 21:42:06 +0530

    All the negative number check added, error is added with throwing exception and error on screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,30 @@ import './App.css'
 function App() {
   const [str, setStr] = useState("")
   const [sum, setSum] = useState(0)
+  const [error, setError] = useState("")
 
 
   function add(numbers: string):number{
-      const num = numbers.match(/-?\d+/g)
+
+      const num = str.match(/-?\d+/g)
+      const negatives : any = []; // Array to collect negative numbers
       if(num == null){
           return 0
       } else{
         const sum = num.reduce((acc, num) => {
           const number = Number(num); // Convert the string to a number
+          if (number < 0) {
+              negatives.push(number); // Collect negative numbers
+          }
           return acc + number;
       }, 0);
+      if(negatives.length > 0){
+        setSum(0)
+        setError(`negative number not allowed ${negatives.join(",")}`) //set
+        throw `negative number not allowed ${negatives.join(",")}`
+      } else{
         return sum
+      }
     }
 }
 
@@ -35,7 +47,7 @@ function stringSum(str : string){
       <button onClick={() => stringSum(str)}>Sum</button>
       </div>
       <div>
-        <p>The sum of the string is {sum}</p>
+        {sum ? <p>The sum of the string is {sum}</p> : <p style={{color: 'red'}}>{error}</p>}
       </div>
     </>
   )
